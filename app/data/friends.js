30580d6a74c637c06cd747e8b3b9f56cac39c9ce
Joshua M. Bedilion <jbedilio@yahoo.com>
2018-01-04 19:59:46 -0500

    updating the friend list

--- app/data/friends.js
+++ app/data/friends.js
@@ -21,7 +21,7 @@ var supers = [
     name:     'Clark Kent',
     alias:    'Superman',
     age:       32,
-    power:    'strength, speed, flight',
+    power:    'strength, speed, flight, x-ray vision, heat-ray vision',
     pic:      'https://media.melty.fr/article-3302985-ajust_1020/j-j-abrams-va-t-il-realiser-man-of-steel.jpg',
     scores:    [
         1,
@@ -38,7 +38,7 @@ var supers = [
     name:      'Peter Parker',
     alias:     'Spiderman',
     age:        21,
-    power:     'strength, speed, sense, webbing, climbing',
+    power:     'strength, speed, extra sensory perception, webbing, climbing',
     pic:       'http://rollingstone.uol.com.br/media/images/medium/2013/02/26/img-1012640-io-espetacular-homem-aranha-2i-600x600.jpg',
     scores:    [
         5,
@@ -55,7 +55,7 @@ var supers = [
     name:      'Tony Stark',
     alias:     'Iron Man',
     age:        37,
-    power:     'iron man suit of armor, flight, speed, strenth, weapons',
+    power:     'iron man suit of armor, flight, speed, strength, weapons',
     pic:       'https://blogs-images.forbes.com/alishagrauso/files/2016/05/Iron-Man-Robert-Downey-Jr-Interview-1200x600.jpg?width=960',
     scores:    [
         5,
@@ -72,7 +72,11 @@ var supers = [
     name:       'Diana Prince',
     alias:      'Wonder Woman',
     age:         27,
-    power:      ''
+    power:      'speed, strength, leaping, laso of truth',
+    pic:        '#',
+    scores:     [
+        
+    ]
 }
 ];
 
