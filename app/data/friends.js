bf1c8bb34734bc2201c04437b34a3b4336c7dd09
Joshua M. Bedilion <jbedilio@yahoo.com>
2018-01-19 19:48:57 -0500

    updating friend JSON file

--- app/data/friends.js
+++ app/data/friends.js
@@ -126,7 +126,14 @@ var supers = [
     power:      'martial arts/ninja, assassin, increased speed & coordination',
     pic:        'http://4.bp.blogspot.com/-2NpV3IkN_MA/UkjYILhiRMI/AAAAAAAAnZs/fvSpH4ECaSM/s1600/Elektra+cinemelodic+garner.jpg',
     scores:     [
-        
+        5,
+        3,
+        5,
+        5,
+        5,
+        5,
+        1,
+        5
     ]
 }
 ];
